eae2f83cbebfbfcce8fb0218d01040e9e3c07dad
Eya Taieb <ETaieb@se.linedata.com>
2020-02-11 15:42:22 +0100

    getLetterGrade Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,13 +22,13 @@ namespace GradeBook.GradeBooks
             var grades = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList();
             
 
-            if (averageGrade > grades[(int)Math.Ceiling(rank * 0.8)-1])
+            if (averageGrade > grades[(int)Math.Ceiling(rank * 0.2)-1])
                 return 'A';
-            else if (averageGrade > grades[(int)Math.Ceiling(rank * 0.6)-1])
-                return 'B';
             else if (averageGrade > grades[(int)Math.Ceiling(rank * 0.4)-1])
+                return 'B';
+            else if (averageGrade > grades[(int)Math.Ceiling(rank * 0.6)-1])
                 return 'C';
-            else if (averageGrade > grades[(int)Math.Ceiling(rank * 0.2)-1])
+            else if (averageGrade > grades[(int)Math.Ceiling(rank * 0.8)-1])
                 return 'D';
             else
                 return 'F';
